106de0c846ce41233959af6be90d25d9c6db9ced
Leia Carter <leiacarts@gmail.com>
2018-11-05 00:27:56 -0500

    books on shelves without hardcoding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Search from './Search';
 import Main from './Main';
-
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  //fetches books
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <Search />
-        <Main />
+        <Main
+          books = {this.state.books}
+        />
       </div>
     )
   }
--- src/main.js
+++ src/main.js
@@ -14,9 +14,17 @@ class Main extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
+                {
+                  this.props.books
+                    .filter(book => book.shelf === 'currentlyReading')
+                    .map(book => (
+                      <li key = { book.id } >
+                        <Book
+                          book = {book}
+                        />
+                      </li>
+                    ))
+                }
                 </ol>
               </div>
             </div>
@@ -24,9 +32,17 @@ class Main extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
+                  {
+                    this.props.books
+                      .filter(book => book.shelf === 'wantToRead')
+                      .map(book => (
+                        <li key = { book.id } >
+                          <Book
+                            book = {book}
+                          />
+                        </li>
+                      ))
+                  }
                 </ol>
               </div>
             </div>
@@ -34,9 +50,17 @@ class Main extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
+                {
+                  this.props.books
+                    .filter(book => book.shelf === 'read')
+                    .map(book => (
+                      <li key = { book.id } >
+                        <Book
+                          book = {book}
+                        />
+                      </li>
+                    ))
+                }
                 </ol>
               </div>
             </div>
